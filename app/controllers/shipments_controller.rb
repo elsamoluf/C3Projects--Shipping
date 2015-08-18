a3fcef0155caaebfd7cd1be68b5d1fee2feb09df
Elsa <elsa.moluf@gmail.com>
2015-08-18 16:24:09 -0700

    Wrote fedex rates in index action.

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -3,7 +3,15 @@ require 'active_shipping'
 class ShipmentsController < ApplicationController
 
 def index
-  render json: {}
+  fedex = ActiveShipping::FedEx.new(login: ENV['ACTIVESHIPPING_FEDEX_LOGIN'], password: ENV['ACTIVESHIPPING_FEDEX_PASSWORD'], key: ENV['ACTIVESHIPPING_FEDEX_KEY'], account: ENV['ACTIVESHIPPING_FEDEX_ACCOUNT'], meter: ENV['ACTIVESHIPPING_FEDEX_METER'], test: true)
+  response = fedex.find_rates(origin, destination, packages)
+  fedex_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+  
+  unless fedex_rates.empty?
+    render json: fedex_rates.as_json, status: :ok
+  else
+    render json: [], status: :no_content
+  end
 end
 
 end
