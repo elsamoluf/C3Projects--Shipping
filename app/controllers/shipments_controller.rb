697e997a93764f0289eea09e63b7a0b9c985d379
Elsa <elsa.moluf@gmail.com>
2015-08-18 17:01:39 -0700

    WIP running tests through actual controller action. Getting location country code error.

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -2,16 +2,25 @@ require 'active_shipping'
 
 class ShipmentsController < ApplicationController
 
-def index
-  fedex = ActiveShipping::FedEx.new(login: ENV['ACTIVESHIPPING_FEDEX_LOGIN'], password: ENV['ACTIVESHIPPING_FEDEX_PASSWORD'], key: ENV['ACTIVESHIPPING_FEDEX_KEY'], account: ENV['ACTIVESHIPPING_FEDEX_ACCOUNT'], meter: ENV['ACTIVESHIPPING_FEDEX_METER'], test: true)
-  response = fedex.find_rates(origin, destination, packages)
-  fedex_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
-  
-  unless fedex_rates.empty?
-    render json: fedex_rates.as_json, status: :ok
-  else
-    render json: [], status: :no_content
+  def index
+    # FEDEX
+    fedex = ActiveShipping::FedEx.new(login: ENV['ACTIVESHIPPING_FEDEX_LOGIN'], password: ENV['ACTIVESHIPPING_FEDEX_PASSWORD'], key: ENV['ACTIVESHIPPING_FEDEX_KEY'], account: ENV['ACTIVESHIPPING_FEDEX_ACCOUNT'], meter: ENV['ACTIVESHIPPING_FEDEX_METER'], test: true)
+    response = fedex.find_rates(params[:origin], params[:destination], params[:packages])
+    fedex_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+
+    # USPS
+    usps = ActiveShipping::USPS.new(login: ENV['ACTIVESHIPPING_USPS_LOGIN'], test: true)
+    response = usps.find_rates(params[:origin], params[:destination], params[:packages])
+    usps_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+
+    # WIP - combine rates arrays?
+    rates = fedex_rates + usps_rates
+
+    unless rates.empty?
+      render json: rates.as_json, status: :ok
+    else
+      render json: [], status: :no_content
+    end
   end
-end
 
 end
--- spec/controllers/shipments_controller_spec.rb
+++ spec/controllers/shipments_controller_spec.rb
@@ -9,29 +9,45 @@ RSpec.describe ShipmentsController, type: :controller do
       ]
     }
     let(:origin) {
-      origin = ActiveShipping::Location.new( country:  'US', state: 'CA', city: 'Beverly Hills', zip: '90210')
+      origin = ActiveShipping::Location.new(country: 'US', state: 'CA', city: 'Beverly Hills', zip: '90210')
     }
     let(:destination) {
-      destination = ActiveShipping::Location.new( country:  'CA', province:  'ON', city:  'Ottawa', postal_code:  'K1P 1J1')
+      destination = ActiveShipping::Location.new(country: 'CA', province: 'ON', city: 'Ottawa', postal_code: 'K1P 1J1')
     }
 
 
     it "is successful" do
-      get :index
+      get :index, origin: origin, destination: destination, packages: packages
+
       expect(response.response_code).to eq 200
     end
 
     it "returns json" do
-      get :index
+      get :index, origin: origin, destination: destination, packages: packages
+
       expect(response.header['Content-Type']).to include 'application/json'
     end
 
-    it "returns rate estimates" do
-      fedex = ActiveShipping::FedEx.new(login: ENV['ACTIVESHIPPING_FEDEX_LOGIN'], password: ENV['ACTIVESHIPPING_FEDEX_PASSWORD'], key: ENV['ACTIVESHIPPING_FEDEX_KEY'], account: ENV['ACTIVESHIPPING_FEDEX_ACCOUNT'], meter: ENV['ACTIVESHIPPING_FEDEX_METER'], test: true)
-      response = fedex.find_rates(origin, destination, packages)
-      fedex_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+    # it "returns fedex rate estimates" do
+    #   fedex = ActiveShipping::FedEx.new(login: ENV['ACTIVESHIPPING_FEDEX_LOGIN'], password: ENV['ACTIVESHIPPING_FEDEX_PASSWORD'], key: ENV['ACTIVESHIPPING_FEDEX_KEY'], account: ENV['ACTIVESHIPPING_FEDEX_ACCOUNT'], meter: ENV['ACTIVESHIPPING_FEDEX_METER'], test: true)
+    #   response = fedex.find_rates(origin, destination, packages)
+    #   fedex_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+    #
+    #   expect(response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}).to eq fedex_rates
+    # end
+    #
+    # it "return usps rate estimates" do
+    #   usps = ActiveShipping::USPS.new(login: ENV['ACTIVESHIPPING_USPS_LOGIN'], test: true)
+    #   response = usps.find_rates(origin, destination, packages)
+    #   usps_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+    #
+    #   expect(response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}).to eq usps_rates
+    # end
 
-      expect(response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}).to eq fedex_rates
+    it "returns both carrier estimates together" do
+      get :index, origin: origin, destination: destination, packages: packages
+      
+      expect(fedex_rates + usps_rates).to eq rates
     end
   end
 
