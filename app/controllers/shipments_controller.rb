8ffb897dc317594057a49977aaf098c9309cf325
Elsa <elsa.moluf@gmail.com>
2015-08-19 09:39:03 -0700

    Clarified WIP approach to accepting petsy data.

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -3,26 +3,30 @@ require 'active_shipping'
 class ShipmentsController < ApplicationController
 
   def index
-    # FEDEX
-    fedex = ActiveShipping::FedEx.new(login: ENV['ACTIVESHIPPING_FEDEX_LOGIN'], password: ENV['ACTIVESHIPPING_FEDEX_PASSWORD'], key: ENV['ACTIVESHIPPING_FEDEX_KEY'], account: ENV['ACTIVESHIPPING_FEDEX_ACCOUNT'], meter: ENV['ACTIVESHIPPING_FEDEX_METER'], test: true)
-
     ### WIP
     # accept betsy address strings, convert into active shipping location objects
     # destination
     state = params[:betsy_address][:state]
     city = params[:betsy_address][:city]
     zip = params[:betsy_address][:zip]
+    destination = ActiveShipping::Location.new(country: 'US', state: state, city: city, zip: zip)
 
     # origin
-    # ???
+    merchant_state = params[:merchant_address][:state]
+    merchant_city = params[:merchant_address][:city]
+    marchant_zip = params[:merchant_address][:zip]
+    origin = ActiveShipping::Location.new(country: 'US', state: merchant_state, city: merchant_city, zip: merchant_zip)
 
     # packages
-    # ???
-
-    destination = ActiveShipping::Location.new(country: 'US', state: state, city: city, zip: zip)
-    # create origin too
-    # and packages
+    product_weight = params[:product_dimensions][:weight] # 5 lb
+    product_dimensions = params[:product_dimensions][:dimensions] # [10l, 10w, 10h]
+    packages = [
+      ActiveShipping::Package.new(product_weight, product_dimensions, units: :imperial)
+    ]
     ###
+
+    # FEDEX
+    fedex = ActiveShipping::FedEx.new(login: ENV['ACTIVESHIPPING_FEDEX_LOGIN'], password: ENV['ACTIVESHIPPING_FEDEX_PASSWORD'], key: ENV['ACTIVESHIPPING_FEDEX_KEY'], account: ENV['ACTIVESHIPPING_FEDEX_ACCOUNT'], meter: ENV['ACTIVESHIPPING_FEDEX_METER'], test: true)
 
     response = fedex.find_rates(origin, destination, packages)
     fedex_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
@@ -32,7 +36,6 @@ class ShipmentsController < ApplicationController
     response = usps.find_rates(origin, destination, packages)
     usps_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
 
-    # WIP - combine rates arrays?
     rates = fedex_rates + usps_rates
 
     unless rates.empty?
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :shipments
+  # resources :shipments
   get '/shipments/:query' => 'shipments#index'
+  post '/shipments/:audit' => 'shipments#create'
+  get '/tracking/:query' => 'shipments#tracking'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
